6322206f9db69a867aa398bfd89882fc63cf5247
Sean <sean@seankerr.com>
2024-08-15 11:00:00 +0000

    add local runner file

---
+++ src/test/health.local.ts
@@ -0,0 +1,10 @@
+import {handler} from '../functions/health/health.handler'
+const eventpayload = {
+    "body": "this is a test",
+} as unknown
+const context = {'test': 'me'}
+import {
+    APIGatewayProxyEventV2,
+  } from "aws-lambda";
+//@ts-ignore
+handler(eventpayload, context).then((response:APIGatewayProxyEventV2) => console.log(response)).catch((error:any) => console.log(error))
